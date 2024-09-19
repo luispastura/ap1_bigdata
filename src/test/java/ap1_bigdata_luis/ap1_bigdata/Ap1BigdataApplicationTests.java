5a9fdebc10e0486dba80cb62067fca1c71ea2c6c
Luis Carlos Pastura Macedo <luispasturamacedo@gmail.com>
2024-09-19 14:42:59 -0300

    criando_ambiente

---
+++ src/test/java/ap1_bigdata_luis/ap1_bigdata/Ap1BigdataApplicationTests.java
@@ -0,0 +1,13 @@
+package ap1_bigdata_luis.ap1_bigdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ap1BigdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
